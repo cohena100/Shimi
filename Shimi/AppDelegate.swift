53ddbe69042642490e9b22c14091e90daccc1dc0
Pango-mac <cohena100@gmail.com>
2017-05-30 10:07:42 +0300

    lighter setup of window when unit testing

--- Shimi/AppDelegate.swift
+++ Shimi/AppDelegate.swift
@@ -15,10 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let vc = MainViewController()
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: vc)
+        setupWindow()
         return true
     }
 
@@ -43,7 +40,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    fileprivate func setupWindow() {
+        var vc = UIViewController()
+        #if DEBUG
+            if (NSClassFromString("XCTest") == nil) {
+                vc = MainViewController()
+            }
+        #else
+            vc = MainViewController()
+        #endif
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+    }
 
 }
 
