c6fc39c02255ca0ca1042211fcd16da3f341ab62
Pango-mac <cohena100@gmail.com>
2017-05-30 08:45:15 +0300

    more tests

--- ShimiTests/RotatingButtonsTests.swift
+++ ShimiTests/RotatingButtonsTests.swift
@@ -9,25 +9,29 @@
 import XCTest
 import RealmSwift
 import RxSwift
+import RxTest
+import RxBlocking
 
 @testable import Shimi
 
 class RotatingButtonsTests: XCTestCase {
     
     var db: Realm!
+    var vm: RotatingButtonsViewModel!
     
     override func setUp() {
         super.setUp()
         self.db = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: self.name))
+        vm = RotatingButtonsViewModel(db: self.db, vc: nil)
     }
     
     override func tearDown() {
         self.db = nil
+        self.vm = nil
         super.tearDown()
     }
     
     func test_isOn_true_1Entry() {
-        let vm = RotatingButtonsViewModel(db: self.db, vc: nil)
         vm.isOn.value = true
         let entries = db.objects(Entry.self).sorted(byKeyPath: "enter", ascending: false)
         XCTAssert(entries.count == 1)
@@ -36,13 +40,27 @@ class RotatingButtonsTests: XCTestCase {
     }
     
     func test_isOn_trueAndFalse_1CompleteEntry() {
-        let vm = RotatingButtonsViewModel(db: self.db, vc: nil)
-        vm.isOn.value = true
-        vm.isOn.value = false
+        createEntries(vm: vm, count: 1)
         let entries = db.objects(Entry.self).sorted(byKeyPath: "enter", ascending: false)
         XCTAssert(entries.count == 1)
         let firstEntry = entries[0]
         XCTAssertNotNil(firstEntry.exit)
+    }
+    
+    func test_isOn_trueAndtrueAndFalse_1CompleteEntry() {
+        vm.isOn.value = true
+        createEntries(vm: vm, count: 1)
+        let entries = db.objects(Entry.self).sorted(byKeyPath: "enter", ascending: false)
+        XCTAssert(entries.count == 1)
+        let firstEntry = entries[0]
+        XCTAssertNotNil(firstEntry.exit)
+    }
+    
+    fileprivate func createEntries(vm: RotatingButtonsViewModel, count: Int) {
+        [0 ..< count].forEach { (index) in
+            vm.isOn.value = true
+            vm.isOn.value = false
+        }
     }
     
 }
